7937a8e45ff20a5a90d0eb5c5ddaa0dc0e2489ef
therobshock <therobshock@yahoo.com>
2019-10-08 13:53:19 -0700

    nav component redesign

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg">
       <div className="container d-flex justify-content-around">
         <a className="small-logo" href="/">
           Home
@@ -15,6 +15,11 @@ function Nav() {
           Home
         </a>
         <a className="main-logo" href="/">
+          <div className="stem"></div>
+          <div className="triangle left-eye"></div>
+          <div className="triangle right-eye"></div>
+          <div className="triangle nose"></div>
+          <div className="mouth"></div>
           Home
         </a>
         <a className="small-logo" href="/">
