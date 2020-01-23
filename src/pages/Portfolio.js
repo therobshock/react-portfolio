bdab254c4a34c25bb8c462a12905f24756ae2bcc
Robert T Bolton <therobshock@gmail.com>
2020-01-23 15:04:00 -0800

    portfolio page data and render

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -11,14 +11,18 @@ class Portfolio extends Component {
         return (
             <Container 
                 fluid
-                flex="d-flex justify-content-around flex-wrap"
-                style={{paddingTop: "6%"}}
+                flex="d-flex justify-content-center flex-wrap"
+                style={{
+                    paddingTop: "6%",
+                    boxSizing: "border-box",
+                    width: "80%"
+                }}
             >
                 {this.state.apps.map(app => (
                     <PortfolioCard
                         id={app.id}
                         key={app.id}
-                        image={app.image}
+                        image={ require('../' + app.image) }
                         title={app.title}
                         about={app.about}
                         link={app.link}
